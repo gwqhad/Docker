910a2fff4d3ba029da4171e616a21788ca5dcccd
seeing-zynq <wenqiang.gao@gzseeing.com>
2019-08-12 09:14:24 +0800

    five commit

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-while [ 1 ]
-do 
-	echo "num"
-	read num 
-	if [ $num -eq 1 ]
-	then
-		exit 0
-	else
-		continue
-	fi
-done
+echo $num
+
+#while [ 1 ]
+#do 
+#	echo "num"
+#	read num 
+#	if [ $num -eq 1 ]
+#	then
+#		exit 0
+#	else
+#		continue
+#	fi
+#done
 
